ba2b0d9aaefbbe6dda75923afb397d85239e5fe2
speedytwenty <speedytwenty@gmail.com>
2015-12-30 00:43:25 -0700

    Cleanup: Removing contrib-able modules, replacing with sandbox projects

--- modules/catalyst/better_migrate/example/README.txt
+++
@@ -1,2 +0,0 @@
-This example leverages the every_field module which is not included. Download
-this module if you want to test with the example.
--- modules/catalyst/menu_lockdown/menu-lockdown-settings.tpl.php
+++
@@ -1,8 +0,0 @@
-<div id="menu-lockdown-settings" class="clearfix">
-  <div class="left">
-    <?php print drupal_render($form['widget']); ?>
-  </div>
-  <div class="right">
-    <?php print drupal_render_children($form); ?>
-  </div>
-</div>
